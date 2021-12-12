dc4fac56680d5bbf972be5acd534724b8dfad931
themordhon <romdhonm12@gmail.com>
2021-12-12 19:09:08 +0700

    event handler - onClick

--- public/app.js
+++ public/app.js
@@ -3,9 +3,16 @@ console.log("ini berjalan dengan baik");
 /* Rendering Dengan ReactJS */
 
 function tick() {
+  function clickAlert() {
+    alert("Hello, ini info dari alert !");
+  }
+
   const element = /*#__PURE__*/React.createElement("div", {
     className: "box"
-  }, /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang"), "/* /* Dibawah ini element yang otomatis di render oleh React */ /* Karena Function yang terdeteksi (time) memungkinkan untuk di render setiap saat */ */", /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
+  }, /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang"), "/* /* Dibawah ini element yang otomatis di render oleh React */ /* Karena Function yang terdeteksi (time) memungkinkan untuk di render setiap saat */ */", /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()), /*#__PURE__*/React.createElement("button", {
+    className: "button",
+    onClick: clickAlert
+  }, "Alert"));
   ReactDOM.render(element, root);
 }
 
--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,9 @@ console.log("ini berjalan dengan baik");
 
 /* Rendering Dengan ReactJS */
 function tick() {
+  function clickAlert() {
+    alert("Hello, ini info dari alert !");
+  }
   const element = (
     <div className="box">
       <h1>Jam Sekarang</h1>
@@ -10,9 +13,11 @@ function tick() {
       Function yang terdeteksi (time) memungkinkan untuk di render setiap saat
       */ */
       <h2>{new Date().toLocaleTimeString()}</h2>
+      <button className="button" onClick={clickAlert}>
+        Alert
+      </button>
     </div>
   );
-
   ReactDOM.render(element, root);
 }
 
